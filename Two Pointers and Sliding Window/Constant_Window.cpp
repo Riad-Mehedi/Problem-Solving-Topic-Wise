2c126c7863cf55ee037e412514f6c7262eec857f
Riad-Mehedi <129571327+Riad-Mehedi@users.noreply.github.com>
2024-11-28 16:38:20 +0600

    Update Constant_Window.cpp

--- Two Pointers and Sliding Window/Constant_Window.cpp
+++ Two Pointers and Sliding Window/Constant_Window.cpp
@@ -3,29 +3,29 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define		ll 			long long 
+#define		ll 		long long 
 #define		lli 		long long int
-#define 	ld 			long double
+#define 	ld 		long double
 
-#define 	nl 			"\n"
-#define     pnl     	cout<<'\n'
+#define 	nl 		"\n"
+#define         pnl     	cout<<'\n'
 #define 	Yes 		cout<<"Yes"<<nl 
-#define 	No 			cout<<"No"<<nl 
+#define 	No 		cout<<"No"<<nl 
 #define 	YES 		cout<<"YES"<<nl 
-#define 	NO 			cout<<"NO"<<nl 
+#define 	NO 		cout<<"NO"<<nl 
 
-#define 	vi 			vector<int>
+#define 	vi 		vector<int>
 #define 	vll 		vector<ll>
-#define 	pb 			push_back
+#define 	pb 		push_back
 
-#define     FAIYAZ  	ios_base::sync_with_stdio(false);cin.tie(NULL);
-#define     srt(v)  	v.begin(),v.end()
-#define     setp(n) 	fixed << setprecision(n)
+#define     FAIYAZ  		ios_base::sync_with_stdio(false);cin.tie(NULL);
+#define     srt(v)  		v.begin(),v.end()
+#define     setp(n) 		fixed << setprecision(n)
 
-#define     debug(x) 	cout << #x << " : " << x << nl;
+#define     debug(x) 		cout << #x << " : " << x << nl;
 
-#define     cs      	cout << "Case " << t << ": ";
-#define     vp(v)   	for(auto it:v){cout << it <<' ';}pnl;
+#define     cs      		cout << "Case " << t << ": ";
+#define     vp(v)   		for(auto it:v){cout << it <<' ';}pnl;
 
 
 int main() {
@@ -56,8 +56,6 @@ int main() {
 
 	return 0;
 }
-
-
 /*
 
 Explaination:
